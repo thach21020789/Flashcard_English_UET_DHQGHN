284a50d6e643e625750b86ff2056646bfd2b219c
21020424DinhTheVuong <100188715+21020424DinhTheVuong@users.noreply.github.com>
2023-04-10 20:38:45 +0700

    Update MiniGame.js

--- Reactjs/src/views/MiniGames/MiniGame.js
+++ Reactjs/src/views/MiniGames/MiniGame.js
@@ -1,9 +1,35 @@
 import React from "react";
+import './MiniGame.scss';
 
 class MiniGame extends React.Component{
-        render(){
-            return(
-                <div>Hello MiniGame</div>
+    componentDidMount() {
+        const script = document.createElement("script");
+        script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
+        script.async = true;
+        document.body.appendChild(script);
+      }
+      useEffect(() => {
+        // This code will run after the component has been rendered
+      }, [])
+
+    play() {
+        var myWords = ["EGG", "MILK", "BUTTER", "JAM", "OATS",
+        "SUGAR", "BREAD", "RUSK"];
+        var tempWord = [];
+        var selectedWord = '';
+        
+    }
+    render(){
+        return(
+            <>
+                <div>
+                    <h1>WORD SEARCH GAME</h1>
+                    <div id = "container">
+                        <div id = "hint"></div>
+                        <div id = "letters"></div>
+                    </div>
+                </div>
+            </>        
         )
     }
 }
