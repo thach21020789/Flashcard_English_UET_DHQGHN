5ed3e181cd206c632443bb557469117176347371
21020424DinhTheVuong <100188715+21020424DinhTheVuong@users.noreply.github.com>
2023-04-15 13:25:48 +0700

    Game 0.0.1

--- Reactjs/src/views/MiniGames/MiniGame.js
+++ Reactjs/src/views/MiniGames/MiniGame.js
@@ -7,26 +7,56 @@ class MiniGame extends React.Component{
         script.src = "https://code.jquery.com/jquery-3.6.0.min.js";
         script.async = true;
         document.body.appendChild(script);
+        // this.setIndividualAttribute(7, "A", "");  
       }
-      useEffect(() => {
-        // This code will run after the component has been rendered
-      }, [])
 
-    play() {
+    render(){
         var myWords = ["EGG", "MILK", "BUTTER", "JAM", "OATS",
         "SUGAR", "BREAD", "RUSK"];
         var tempWord = [];
         var selectedWord = '';
         
-    }
-    render(){
+
+
+        const rows = 12;
+        const columns = 12;
+        const individualElements = [];
+
+        for (let i = 1; i <= rows; i++) {
+            for (let j = 1; j <= columns; j++) {
+              individualElements.push(
+                <div className="individual" data-row={i} data-column={j} data={"A"} key={`${i}-${j}-${"A"}`} />
+              );
+            }
+        }
+
+        var positions = ["row", "column"];
+        var nextLetter = 0;
+        var newStart = 0;
+
+        for (let i = 0; i<myWords.length; i++) {
+            const orientation = positions[Math.floor(Math.random() * positions.length)];
+            // console.log(orientation);
+            const start = Math.floor(Math.random() * individualElements.length);
+            // const individualElement = this.individualElements[start].querySelector('.individual');
+            //const rowData = this.individualElements[start].querySelector('.individual').dataset.row;
+                        // const myColumn = parseInt(individualElements[start].querySelector.dataset.row);
+
+        }
         return(
             <>
                 <div>
                     <h1>WORD SEARCH GAME</h1>
                     <div id = "container">
-                        <div id = "hint"></div>
-                        <div id = "letters"></div>
+                        <div id = "hint">
+                            {myWords.map((word, index) => (
+                                <p key={index}>{word}</p>
+                            ))}
+                        </div>
+
+                        <div id = "letters" ref={this.individualElementsRef}>
+                            {individualElements}
+                        </div>
                     </div>
                 </div>
             </>        
@@ -35,3 +65,5 @@ class MiniGame extends React.Component{
 }
 
 export default MiniGame;
+
+
